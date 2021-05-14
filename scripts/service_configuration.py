11e8d854e0503b21159e23b0f414be9f47897017
greencrow <slonik256@yandex.ru>
2021-05-14 20:02:18 +0300

    version 0.0.5

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -3,8 +3,8 @@
 import time
 import tqdm
 
-configuration_number = "0xAB0A"
-version = "0.0.4"
+configuration_number = "0x90ABF0A78"
+version = "0.0.5"
 
 print("Service package 2: ver. {}".format(version))
 time.sleep(0.5)
