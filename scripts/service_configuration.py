973a6317fda2b2a91f94f3ab36f57224acb03e41
NikolayIvanovWS <kolyan_ivanov@list.ru>
2023-06-07 17:58:34 +0300

    Add branch new

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -3,8 +3,8 @@
 import time
 import tqdm
 
-configuration_number = "1xASWD890A"
-version = "1.0.0"
+configuration_number = "N9jLQ1"
+version = "1.1.0"
 
 print("Service package 2: ver. {}".format(version))
 time.sleep(0.5)
@@ -12,14 +12,14 @@ print("Service package 2: start configuration ")
 time.sleep(0.5)
 print("Service package 2: stage 1")
 time.sleep(0.5)
-for i in tqdm.tqdm(range(1000), ascii=True, desc="Finding dependencies"):
+for i in tqdm.tqdm(range(2000), ascii=True, desc="Finding dependencies"):
     time.sleep(0.01)
 print("Service package 2: stage 1 finished")
 time.sleep(0.5)
 print("Service package 2: stage 2")
 time.sleep(1.5)
 print("Service package 2: configuration package 1 found")
-for i in tqdm.tqdm(range(10000), ascii=True, desc="Setup in progress"):
+for i in tqdm.tqdm(range(5000), ascii=True, desc="Setup in progress"):
     time.sleep(0.001)
 print("Service package 2: stage 2 finished")
 time.sleep(0.5)
